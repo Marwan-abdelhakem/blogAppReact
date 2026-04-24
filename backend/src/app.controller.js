e0e68ece08a01515dcc9f49d128e96fe875cf4e3
Marwan abdelhakem <marwanabdelhakem@gmail.com>
2026-04-24 14:14:50 +0300

    update

--- backend/src/app.controller.js
+++ backend/src/app.controller.js
@@ -8,7 +8,7 @@ import globalErrorHandler from "./Utlis/errorHandler.utlis.js"
 
 const bootStrap = async (app, express) => {
     app.use(cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+        origin: 'blog-app-react-steel.vercel.app' || "http://localhost:5173",
         credentials: true,
     }))
 
